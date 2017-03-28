d148f11b585cb6799c347a636835f38f6eb308c2
George Higbie <gehigbie@gmail.com>
2017-03-28 14:58:20 -0700

    add more routes and a json

--- server.js
+++ server.js
@@ -3,7 +3,31 @@ const express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello Express!</h1>");
+    //res.send("<h1>Hello Express!</h1>");
+    res.send({
+        name: "George",
+        likes: [
+            "Running",
+            "Biking",
+            "Greek Food",
+            "New Places",
+            "Weight Training"
+        ]
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Page!</h1>")
+});
+
+app.get("/moreinfo",  (req, res) => {
+    res.send("<h1>There's a lot more information on this page</h1>");
+});
+
+app.get("/bad" (req, res) => {
+    res.send({
+        error: "There was an error requesting this page"
+    });
 });
 
 app.listen(3000, process.env.IP, () => {
