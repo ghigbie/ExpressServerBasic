3dc4587dc9498d3f0a6daeaae189d9927c76fc40
George Higbie <gehigbie@gmail.com>
2017-03-28 14:47:47 -0700

    add basic html to send

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Express!");
+    res.send("<h1>Hello Express!</h1>");
 });
 
 app.listen(3000, process.env.IP, () => {
