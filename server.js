633474a5bf7111225e7b59591bc8fc526e23d03a
George Higbie <gehigbie@gmail.com>
2017-03-28 14:42:37 -0700

    add callback function to .listen

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const express = require("express");
 var app = express();
 
 app.get("/", (req, res) => {
+    res.send("Hello Express!");
+});
 
+app.listen(3000, process.env.IP, () => {
+    console.log("Server is running");
 });
