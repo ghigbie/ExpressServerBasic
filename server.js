213da91a0db7545caf87bc368e0bc09b9b947e12
George Higbie <gehigbie@gmail.com>
2017-03-28 17:50:35 -0700

    add hbs Helper and apply to home.hbs

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ hbs.registerHelper("getCurrentYear", () => {
     return new Date().getFullYear();
 });
 
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+})
 app.get("/", (req, res) => {
     res.render("home.hbs", {
         pageTitle: "Welcome Page",
